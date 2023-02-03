74fa0d451a6ebea3349b9732a9adee8159bbca40
MattDaigle93 <matttdaigle@gmail.com>
2023-02-03 10:34:03 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using SimpleTrader.EntityFramework;
+using SimpleTrader.EntityFramework.Services;
+
+IDataService<User> userService = new GenericDataService<User>(new SimpleTraderDbContextFactory());
+Console.WriteLine(userService.Delete(7).Result);
