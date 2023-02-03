a55561d4ebe83dfeef372690be88f3b803160540
MattDaigle93 <matttdaigle@gmail.com>
2023-02-03 12:08:40 -0500

    Started WPF-side of application

---
+++ SimpleTrader.WPF/Models/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.Models
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string proptertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(proptertyName));
+        }
+    }
+}
